d0cbed878867910cf1723e5d61a2eaa4ce97b0ba
Imad Atyat-Allah <imadatyatalah@gmail.com>
2022-10-02 15:38:32 +0100

    refactor: switch to ts

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,4 +1,9 @@
-export default async function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
   }
